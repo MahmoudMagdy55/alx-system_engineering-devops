c751346fe39b2e0f4808df1acde0a61cb5801f32
MahmoudMagdy55 <Email.com>
2023-06-15 08:36:26 -0700

    --

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point 
+ *
+ * Description : C program
+ *
+ * Return : Alwats 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
